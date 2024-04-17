9204fadb1ad11459cdc5386865ae31baf8bc8d21
Pablo-Cogo <pablocogo1@gmail.com>
2024-04-17 19:22:50 -0300

    remove-dist

--- api/dist/src/dto/retornoTokenDTo.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TypeToken;
-(function (TypeToken) {
-    TypeToken["access_token"] = "access_token";
-    TypeToken["refresh_token"] = "refresh_token";
-})(TypeToken || (TypeToken = {}));
-//# sourceMappingURL=retornoTokenDTo.js.map
--- api/dist/src/services/google.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class GoogleAuthService {
-}
-exports.default = GoogleAuthService;
-//# sourceMappingURL=google.js.map
